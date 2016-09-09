43459152430e698c5f00e1d22c732feb489bca66
feroxx <frhatfrat@gmail.com>
2016-09-10 00:24:28 +0300

    Update Android.mk

--- proprietary/vendor/lib/Android.mk
+++ proprietary/vendor/lib/Android.mk
@@ -17,15 +17,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter u2,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libril
-LOCAL_SRC_FILES := libril.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_SRC_FILES := libtime_genoff.so
 LOCAL_MODULE_TAGS := optional
