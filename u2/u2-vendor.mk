7a4514619e10b0026ac560625e79bb4dd8e7b0a7
feroxx <frhatfrat@gmail.com>
2018-01-22 01:48:51 +0300

    fix again

--- u2/u2-vendor.mk
+++ u2/u2-vendor.mk
@@ -16,10 +16,9 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/etc/acdbdata,system/etc/acdbdata) \
     $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/etc/data,system/etc/data) \
     $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/etc/firmware,system/etc/firmware) \
-	$(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/etc/gn_camera_feature,system/etc/gn_camera_feature) \
-    $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/etc/permissions,system/etc/permissions) \
+    $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/etc/gn_camera_feature,system/etc/gn_camera_feature) \
     $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/lib,system/lib) \
-	$(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/lib/hw,system/lib/hw) \
+    $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/lib/hw,system/lib/hw) \
     $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/bin,system/bin) \
     $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/vendor/lib/egl,system/vendor/lib/egl) \
     $(call find-copy-subdir-files,*,vendor/iuni/u2/proprietary/vendor/lib/hw,system/vendor/lib/hw) \
